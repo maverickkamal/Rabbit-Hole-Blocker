3b42566dd43d6493d63d6fd8d6b0485fb13f7fde
maverickkamal <musakamalmuhammad@gmail.com>
2026-03-04 00:15:32 +0100

    Add core logic to inject/remove CSS on YouTube/Reddit based on blocker state and tab updates.

--- background.js
+++ background.js
@@ -1,1 +1,51 @@
+const MATCH_PATTERNS = ['*://*.youtube.com/*', '*://*.reddit.com/*'];
 
+function isTargetUrl(url) {
+    return /^https?:\/\/(www\.)?(youtube\.com|reddit\.com)/i.test(url);
+}
+
+function injectCSS(tabId) {
+    chrome.scripting.insertCSS({
+        target: { tabId: tabId },
+        files: ['blocker.css']
+    }).catch(function () { });
+}
+
+function removeCSS(tabId) {
+    chrome.scripting.removeCSS({
+        target: { tabId: tabId },
+        files: ['blocker.css']
+    }).catch(function () { });
+}
+
+function applyToAllTabs(enabled) {
+    chrome.tabs.query({ url: MATCH_PATTERNS }, function (tabs) {
+        tabs.forEach(function (tab) {
+            if (enabled) {
+                injectCSS(tab.id);
+            } else {
+                removeCSS(tab.id);
+            }
+        });
+    });
+}
+
+chrome.runtime.onInstalled.addListener(function () {
+    chrome.storage.local.set({ enabled: true });
+});
+
+chrome.storage.onChanged.addListener(function (changes) {
+    if (changes.enabled) {
+        applyToAllTabs(changes.enabled.newValue);
+    }
+});
+
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    if (changeInfo.status === 'complete' && tab.url && isTargetUrl(tab.url)) {
+        chrome.storage.local.get({ enabled: true }, function (data) {
+            if (data.enabled) {
+                injectCSS(tabId);
+            }
+        });
+    }
+});
